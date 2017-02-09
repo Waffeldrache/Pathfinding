2f793f80f08dbf16f80899a2006c19350ce158d9
Kronos197 <LeonieB.1998>
2017-02-09 21:44:06 +0100

    Added Main Class

---
+++ LadderGame/src/main/java/de/kronos197/pathfinding/LadderGame.java
@@ -0,0 +1,22 @@
+package de.kronos197.pathfinding;
+
+/**
+ * Created by lenna on 09.02.2017.
+ */
+public class LadderGame
+{
+    public static void main(String[] args)
+    {
+        new LadderGame(args);
+    }
+
+    private String[] args;
+
+    public LadderGame(String[] args)
+    {
+        this.args = args;
+
+
+    }
+
+}
